e3e36fe3b5534d57c5fd121de59cebb7c75a017b
mguidoti <marcus.guidoti@gmail.com>
2020-11-09 08:53:30 -0300

    feat: Code for "Debugando NodeJS" class

--- src/server.ts
+++ src/server.ts
@@ -5,9 +5,9 @@ import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) =>
-  response.json({ message: 'Hello GoStack!' }),
-);
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(3333, () => {
   // eslint-disable-next-line no-console
