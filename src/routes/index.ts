83013c92e09d0faeaf05706796d37a745970d880
mguidoti <marcus.guidoti@gmail.com>
2020-11-15 22:36:16 -0300

    feat: Code for "Validando credenciais" class

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 // src/routes/index.ts
 import { Router } from 'express';
 import appointmentsRouter from './appointments.routes';
+import sessionsRouter from './sessions.routes';
 import usersRouter from './users.routes';
 
 const routes = Router();
@@ -9,5 +10,7 @@ const routes = Router();
 routes.use('/appointments', appointmentsRouter);
 
 routes.use('/users', usersRouter);
+
+routes.use('/sessions', sessionsRouter);
 
 export default routes;
