e7c468d00bf0135bec659c8202d8ba7df19944f0
mguidoti <marcus.guidoti@gmail.com>
2020-11-16 09:13:01 -0300

    feat: Code for "Servindo arquivos estaticos" class

--- src/server.ts
+++ src/server.ts
@@ -6,11 +6,15 @@ import routes from './routes';
 import './database';
 import 'reflect-metadata';
 
+import uploadConfig from './config/upload';
+
 const app = express();
 
 app.use(express.json());
 
 app.use(routes);
+
+app.use('/files', express.static(uploadConfig.directory));
 
 app.listen(3333, () => {
   // eslint-disable-next-line no-console
